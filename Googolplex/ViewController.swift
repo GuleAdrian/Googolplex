10b9f887565e01589927ff1d968125ccc7881412
Adrian Evensen <guleadrian@gmail.com>
2016-06-07 14:38:21 +0200

    Initial Commit

---
+++ Googolplex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Googolplex
+//
+//  Created by Adrian Evensen on 07/06/16.
+//  Copyright © 2016 Adrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
